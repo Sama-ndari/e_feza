dcc89424c84c5b46164383aa8c5ef4136f5956de
Lacasse <Junior_julescesar10@hotmail.com>
2024-02-14 13:40:24 +0200

    last commit

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def generate():
     format_choisi = request.form["format"]
 
     # Enregistrer la photo du user sur le serveur
-    # photo_path = os.path.join("static", "uploads", photo.filename)
     photo_path = f"static/{photo.filename}"
     photo.save(photo_path)
 
@@ -33,68 +32,58 @@ def generate():
     if format_choisi == "pdf":
         return send_file(badge, as_attachment=True)
     else:
-        # Enregistrer la badge généré en tant qu'image
-        badge_path = os.path.join("static/badge.png")
-        badge.save(badge_path)
         return send_file("static/badge.png", as_attachment=True)
 
 
 def generate_badge(nom, prenom, code_agent, numero_telephone, photo_path, format_):
+    # on cree la badge
+    badge = generate_badge_image(nom, prenom, code_agent, numero_telephone, photo_path)
     if format_ == "pdf":
-        # le badge au format PDF
+        # recuperer l'image de la badge
         pdf_path = os.path.join("static", "badge.pdf")
         c = canvas.Canvas(pdf_path, pagesize=landscape(A6))
 
         # Dessiner le badge dans le canvas PDF
-        c.drawImage(photo_path, 40, 140, width=120, height=150)
-        c.drawImage("static/images/logo.png", 300, 240, width=120, height=60)
-
-        font_petit = "Helvetica", 14
-        font_grand = "Helvetica-Bold", 24
-        blue1_color = (0, 101, 148)
-        blue2_color = (0, 176, 240)
-        text_x = 200
-        text_y = 220
-        c.setFont(*font_grand)
-        c.setFillColor(blue2_color)
-        c.drawString(text_x, text_y, nom)
-        c.setFont(*font_petit)
-        c.setFillColor(blue1_color)
-        c.drawString(text_x, text_y - 30, prenom)
-        c.drawString(text_x, text_y - 60, f"Code Agent: {code_agent}")
-        c.drawString(text_x, text_y - 80, f"📞 {numero_telephone}")
+        photo = "static/badge.png"
+        c.drawImage(photo, 10, 50, width=319, height=232)
 
         c.showPage()
         c.save()
         return pdf_path
     else:
-        # Charger le modèle de badge, l image donc
-        badge = Image.open("static/images/badge_temp.png")
-        draw = ImageDraw.Draw(badge)
-
-        font_petit = ImageFont.truetype("arial.ttf", size=20)
-        font_grand = ImageFont.truetype("arial.ttf", size=36)
-        blue1_color = (0, 101, 148)
-        blue2_color = (8, 53, 83)
-        # blue3_color = (68, 158, 215)
-        text_x = 450
-        text_y = 500
-        draw.text((text_x, text_y), f"{nom}", font=font_grand, fill=blue1_color)
-        draw.text((text_x + 10, text_y + 40), f"{prenom}", font=font_petit, fill=blue2_color)
-        draw.text((text_x, text_y + 80), f"Code Agent: {code_agent}", font=font_petit, fill=blue2_color)
-        draw.text((text_x, text_y + 110), f"📞 {numero_telephone}", font=font_petit, fill=blue2_color)
-
-        # Ajout de la photo du user sur la badge
-        user_pic = Image.open(photo_path)
-        resized_pic = user_pic.resize((200, 250))
-        badge.paste(resized_pic, (200, 380))
-
-        # Ajout du logo de eFeza sur la badge
-        logo = Image.open("static/images/logo.png")
-        resize_logo = logo.resize((200, 100))
-        badge.paste(resize_logo, (450, 380))
-
         return badge
+
+
+def generate_badge_image(nom, prenom, code_agent, numero_telephone, photo_path):
+    # Charger le modèle de badge, l image donc
+    badge = Image.open("static/images/badge.jpg")
+    draw = ImageDraw.Draw(badge)
+
+    font_petit = ImageFont.truetype("oswald.light.ttf", size=15)
+    font_grand = ImageFont.truetype("oswald.bold.ttf", size=22)
+    blue1_color = (0, 101, 148)
+    blue2_color = (8, 53, 83)
+    text_x = 145
+    text_y = 70
+    draw.text((text_x, text_y - 2), f"{nom}", font=font_grand, fill=blue1_color)
+    draw.text((text_x, text_y + 33), f"{prenom}", font=font_petit, fill=blue2_color)
+    draw.text((text_x, text_y + 59), f"Code Agent: {code_agent}", font=font_petit, fill=blue2_color)
+    draw.text((text_x, text_y + 86), f"📞 {numero_telephone}", font=font_petit, fill=blue2_color)
+
+    # Ajout de la photo du user sur la badge
+    user_pic = Image.open(photo_path)
+    resized_pic = user_pic.resize((105, 123))
+    badge.paste(resized_pic, (27, 79))
+
+    # Ajout du logo de eFeza sur la badge
+    logo = Image.open("static/images/logo_badge.png")
+    resize_logo = logo.resize((100, 40))
+    badge.paste(resize_logo, (215, 180))
+
+    # Enregistrer la badge généré en tant qu'image
+    badge_path = os.path.join("static/badge.png")
+    badge.save(badge_path)
+    return badge
 
 
 if __name__ == "__main__":
